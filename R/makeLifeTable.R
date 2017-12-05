463bcb84058952a4ac93ecfa39fee774c9030792
Rob Salguero-Gomez <r.salguero@uq.edu.au>
2017-12-05 13:28:14 +0100

    Added functionality to makeLifeTable to include dx, qx, and all other things x

--- R/makeLifeTable.R
+++ R/makeLifeTable.R
@@ -1,4 +1,4 @@
-#' A function to produce a life table from a matrix population model
+#' Produces a life table from a matrix population model
 #' 
 #' This function uses age-from-stage decompositions to calculate a life table
 #' from a matrix population model
@@ -6,30 +6,31 @@
 #' A detailed description of these methods can be found in section 5.3 of
 #' Caswell (2001) and the supplementary information of Jones et al. (2014).
 #' 
-#' @param matU The U matrix - survival-dependent transitions (e.g. changes in
-#' size/ageing/development)
-#' @param matF The F matrix - sexual reproduction
-#' @param matC The C matrix - clonal reproduction
-#' @param startLife The stage of of the life cycle that structures the matrix
-#' population model where life is considered to begin. This is usually 1 (i.e.
-#' the first stage), but in species with a permanent propagule bank (e.g.
-#' seedbank, or similar), users may wish to use the first non-permanent
-#' propagule bank stage along the life cycle.
-#' @param nSteps Number of time steps for which the life table be constructed.
+#' @param matU A matrix containing only survival-dependent processes (e.g. progression,
+#' stasis, retrogression).
+#' @param matF A matrix containing only sexual reproduction, with zeros
+#' elsewhere.
+#' @param matC A matrix containing only clonal reproduction, with zeros
+#' elsewhere. If not provided, it defaults to a matrix with all zeros.
+#' @param startLife The first stage at which the author considers the beginning
+#' of life in the life cycle of the species. It defaults to the first stage.
+#' @param nSteps Number of time steps for which the life table will be calculated.
 #' This is on the same units as the matrix population model - see
 #' MatrixPeriodicity in metadata of COMPADRE/COMADRE.
-#' @return A `data.frame` with between 2 and 4 columns. If just `matU` is
-#' provided, the `data.frame` has columns `x` (age), `lx` (survivorship).  If
-#' `matF` is provided (in addition to `matU`), `mx` (age-specific sexual
-#' reproduction) is included in the output `data.frame`. Likewise, if `matC` is
-#' provided, an addition column, `cx` (age-specific clonal reproduction) is
-#' included.
+#' @return A `data.frame` with between 5 and 8 columns. If just `matU` is
+#' provided, the `data.frame` has columns `x` (age), `lx` (survivorship), 'dx' (proportion of
+#' the original cohort dying during each stage), 'qx' (force of mortality), and 'ex'
+#' (age-specific life expectancy).
+#' If `matF` is provided (in addition to `matU`), `mx` (age-specific sexual
+#' reproduction) and lxmx (# sexually-produced recruits per capita in each age) are included
+#' in the output.
+#' If `matC` is provided (in addition to `matU`), `cx` (age-specific clonal
+#' reproduction) and lxcx (# clonally-produced recruits per capita in each age) are included
+#' in the output.
 #' @note %% ~~further notes~~
-#' @author Roberto Salguero-Gómez <rob.salguero@@sheffield.ac.uk>
-#' 
-#' Hal Caswell <h.caswell@@uva.nl>
-#' 
-#' Owen R. Jones <jones@@biology.sdu.dk>
+#' @author Roberto Salguero-Gómez <rob.salguero@zoo.ox.ac.uk>
+#' Hal Caswell <h.caswell@uva.nl>
+#' Owen R. Jones <jones@biology.sdu.dk>
 #' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
 #' @references
 #' 
@@ -55,6 +56,14 @@
 #' @export makeLifeTable
 makeLifeTable <- function(matU, matF = NULL, matC = NULL, startLife = 1, nSteps = 1000){
   
+  #Error checks
+  if (dim(matU)[1]!=dim(matU)[2]) stop("Your matrix population model is not a square matrix")
+  if (length(which(colSums(matU)>1))>0) print("Warning: matU has at least one stage-specific survival value > 1")
+  if (any(is.na(matF))) {matF[is.na(matF)]=0
+                        print("NAs exist in matF. These have been zero-ed")}
+  if (any(is.na(matC))) {matC[is.na(matC)]=0
+                        print("NAs exist in matC. These have been zero-ed")}
+  
   matDim = ncol(matU)
   
   #Age-specific survivorship (lx) (See top function on page 120 in Caswell 2001):
@@ -67,11 +76,34 @@ makeLifeTable <- function(matU, matF = NULL, matC = NULL, startLife = 1, nSteps 
   
   lx = survivorship[, startLife]
   lx = c(1, lx[1:(length(lx) - 1)])
+  
+  #Proportion of original cohort dying during each age
+  dx = c(lx[1:(length(lx)-1)]-lx[2:length(lx)], NA)
+  
+  #Force of mortality
+  qx = dx/lx
+  
+  #Average proportion of individuals alive at the middle of a given age
+  Lx = (lx[1:(length(lx)-1)] - lx[2:length(lx)])/2
+  
+  #Total number of individuals alive at a given age and beyond
+  Tx = cumsum(Lx)
 
-  #Make room for dx and qx under assumption of 0.5 in age gap distributions
+  #Mean life expectancy conditional to a given age:
+  ex = Tx/lx[1:(nSteps-1)]
+  
+  for (j in 1:(nSteps-1)){
+    if (is.infinite(ex[j])) ex[j]=NA
+  }
   
   #Start to assemble output object
-  out = data.frame(x = 0:(length(lx)-1),lx = lx)
+  out = list(x = 0:(length(lx)-1),
+                   lx = lx,
+                   dx = dx,
+                   qx = qx,
+                   Lx = Lx,
+                   Tx = Tx,
+                   ex = ex)
   
   if(!missing(matF)){
     if(sum(matF,na.rm=T)==0){
@@ -89,7 +121,15 @@ makeLifeTable <- function(matU, matF = NULL, matC = NULL, startLife = 1, nSteps 
   }  
   mx = ageFertility[, startLife]
   mx = c(0, mx[1:(length(mx) - 1)])
+  mx[is.nan(mx)]=0
   out$mx = mx
+  out$lxmx = lx*mx
+  
+  #Net reproductive value
+  out$R0Fec = sum(out$lxmx)
+  
+  #Generation time
+  out$TcFec = sum(out$x*out$lxmx)/sum(out$lxmx)
   }
   
   if(!missing(matC)){
@@ -108,7 +148,27 @@ makeLifeTable <- function(matU, matF = NULL, matC = NULL, startLife = 1, nSteps 
   }  
   cx = ageClonality[, startLife]
   cx = c(0, cx[1:(length(cx) - 1)])
+  cx[is.nan(cx)]=0
   out$cx = cx
+  out$lxcx = lx*cx
+  
+  #Net reproductive value
+  out$R0Clo = sum(out$lxcx)
+  
+  #Generation time
+  out$TcClo = sum(out$x*out$lxcx)/sum(out$lxcx)
+  }
+  
+  if(!missing(matF) & !missing(matC)){
+    mxcx=mx+cx
+    out$mxcx=mxcx
+    out$lxmxcx=lx*mxcx
+    
+    #Net reproductive value
+    out$R0FecClo = sum(out$lxmxcx)
+    
+    #Generation time
+    out$TcFecClo=sum(out$x*out$lxmxcx)/sum(out$lxmxcx)
   }
   
   return(out)
