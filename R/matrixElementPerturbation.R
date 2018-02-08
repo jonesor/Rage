f6b3bffa1f6c69467968acd7bb2a7e8762dd557a
levisc8 <levisc8@gmail.com>
2018-02-08 20:09:53 +0100

    Document previously undocumented demogstat

--- R/matrixElementPerturbation.R
+++ R/matrixElementPerturbation.R
@@ -1,11 +1,18 @@
-#' A function to perform element perturbation of a matrix population model for any demographic statistic.
+#' A function to perform element perturbation of a matrix population model for 
+#' any demographic statistic.
 #' 
-#' A function to perform element perturbation of a matrix population model and measure the response of the per-capita population growth rate at equilibrium (λ) or (with a user-supplied function) any other demographic statistic.
+#' A function to perform element perturbation of a matrix population model and
+#' measure the response of the per-capita population growth rate at equilibrium (λ)
+#' or (with a user-supplied function) any other demographic statistic.
 #' 
 #' @param matU The U matrix (processes related to survival, growth and retrogression).
 #' @param matF The F matrix (sexual reproduction processes).
 #' @param matC The C matrix (clonal reproduction processes).
 #' @param pert Perturbation parameter.
+#' @param demogstat A character string that is the name of a function.
+#' The default is the the per-capita population growth rate at equilibrium (λ). 
+#'  Also accepts a user-supplied function that performs a calculation on
+#'  a projection matrix and returns a single numeric value.
 #' @param ... Additional arguments passed to the function \code{demogstat}.
 #' @return %% ~Describe the value returned 
 #' @note %% ~~further notes~~
