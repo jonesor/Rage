db7c11b2b38c964e9a65df73119e8b6d266d99f2
patrickbarks <patrick.barks@gmail.com>
2018-02-27 13:41:50 +0100

    Edits to language and formatting documentation

--- R/longevity.R
+++ R/longevity.R
@@ -1,43 +1,44 @@
-#' A function to calculate measures of longevity.
-#' 
+#' Calculate measures of life expectancy and longevity
+#'
 #' A function to calculate life expectancy and maximum longevity of individuals
 #' in a matrix population model
-#' 
+#'
 #' This function applies Markov chain approaches to obtain the mean and variance
 #' in life expectancy, and a loop to calculate maximum longevity.
-#' 
+#'
 #' @param matU A matrix containing only survival-dependent processes (growth,
-#' stasis, shrinkage).
+#'   stasis, shrinkage).
 #' @param startLife The first stage at which the author consider the beginning
-#' of life.
+#'   of life.
 #' @param initPop Initial population size.
-#' @param run Number of iterations that the maximum longevity will be
-#' calculated for.
-#' @return This function applies Markov chain approaches to obtain mean life
-#' expectancy, and a loop to calculate maximum longevity. Outputs are:
-#' 
-#' - 'eta': mean life expectancy conditional on entering the life cycle on life
-#' stage described by 'startLife'.
-#' 
-#' - 'var_eta': variance in life expectancy conditional on entering the life
-#' cycle on life stage described by 'startLife'.
-#' 
-#' - 'Max': maximum longevity observed by iterating a population vector with
-#' 'initPop' individuals in stage 'startLife' up to 'run' times.
+#' @param run Number of iterations that maximum longevity will be calculated
+#'   over.
+#' @return Returns a list including the following named elements (each of which
+#'   contain a single numeric value):
+#'
+#'   - \code{eta}: mean life expectancy conditional on entering the life cycle
+#'   at life stage described by \code{startLife}
+#'
+#'   - \code{var_eta}: variance in life expectancy conditional on entering the
+#'   life cycle at life stage described by \code{startLife}
+#'
+#'   - \code{Max}: maximum longevity observed by iterating a population vector
+#'   with \code{initPop} individuals in stage \code{startLife} up to \code{run}
+#'   times
 #' @note %% ~~further notes~~
 #' @author Roberto Salguero-Gomez <rob.salguero@@zoo.ox.ac.uk>
 #' @author Hal Caswell <hcaswell@@whoi.edu>
 #' @references Caswell, H. (2001) Matrix Population Models: Construction,
-#' Analysis, and Interpretation. Sinauer Associates; 2nd edition. ISBN:
-#' 978-0878930968
-#' 
-#' Morris, W. F., and D. F. Doak. 2003. Quantitative Conservation 
-#' Biology: Theory and Practice of Population Viability Analysis. 
-#' Sinauer Associates, Sunderland, Massachusetts, USA
+#'   Analysis, and Interpretation. Sinauer Associates; 2nd edition. ISBN:
+#'   978-0878930968
+#'
+#'   Morris, W. F., and D. F. Doak. 2003. Quantitative Conservation Biology:
+#'   Theory and Practice of Population Viability Analysis. Sinauer Associates,
+#'   Sunderland, Massachusetts, USA
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-#' 
-#' matU <- matrix (c(0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0.3, 0, 0, 0, 0, 0.1, 0.1), nrow = 4, byrow = TRUE)
+#' matU <- matrix(c(0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0.3, 0, 0, 0, 0, 0.1, 0.1),
+#'                nrow = 4, byrow = TRUE)
 #' 
 #' longevity(matU, startLife = 1, initPop = 100, run = 1000)
 #' 
