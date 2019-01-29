90c39727f30ee67159bf38e5415ccaebffc84aec
patrickbarks <patrick.barks@gmail.com>
2019-01-29 16:01:42 +0100

    standardize args and update doc

--- R/vr_vec.R
+++ R/vr_vec.R
@@ -1,11 +1,11 @@
 #' Derive stage-specific vital rates from a matrix population model
 #' 
 #' @description 
-#' Derive stage-specific vital rates from a matrix population model. Available
-#' vital rates include survival, and a variety of traits that are conditional on
-#' survival including growth, shrinkage, stasis, entering and exiting dormancy,
-#' and reproduction. Vital rates corresponding to impossible transitions are
-#' coerced to \code{NA}.
+#' Derive a vector of stage-specific vital rates from a matrix population model.
+#' Available vital rates include survival, and a variety of traits that are
+#' conditional on survival including growth, shrinkage, stasis, entering and
+#' exiting dormancy, and reproduction. Vital rates corresponding to impossible
+#' transitions are coerced to \code{NA}.
 #' 
 #' With one exception, these functions assume that transition rates in the
 #' matrix population model were in fact calculated as the product of
@@ -29,12 +29,15 @@
 #'   elements indicating whether a given \code{matR} transition is possible
 #'   (\code{TRUE}) or not (\code{FALSE}). Defaults to \code{matR > 0} (see
 #'   Details).
-#' @param stage_exclude Integer indicator(s) for stages that should be excluded
-#'   from the calculation of vital rates. Transitions both to and from the
-#'   indicated stage will be set to \code{NA}.
+#' @param exclude_row Integer or logical vector indicating stages for which
+#'   transitions \emph{to} the stage should be excluded from the calculation of
+#'   vital rates. See section \emph{Excluding stages}.
+#' @param exclude_col Integer or logical vector indicating stages for which
+#'   transitions \emph{from} the stage should be ignore (coerced to \code{NA}).
+#'   See section \emph{Excluding stages}.
 #' @param dorm_stages Integer indicator(s) for dormant stage classes.
-#' @param weights Vector of stage-specific weights to apply while averaging
-#'   vital rates.
+#' @param weights_row Vector of stage-specific weights to apply while averaging
+#'   vital rates across rows within columns. See section \emph{Weighting}.
 #' @param surv_only_na If there is only one possible \code{matU} transition in a
 #'   given column, should that transition be attributed exclusively to survival?
 #'   If \code{TRUE}, the vital rate of growth/stasis/shrinkage in that column
@@ -62,6 +65,17 @@
 #' population model includes transitions that are estimated to be \code{0} but
 #' still in fact possible, one should specify the \code{posU} and/or \code{posR}
 #' arguments manually.
+#' 
+#' @section Excluding stages:
+#' It may be desirable to exclude one or more stages from the calculation of
+#' certain vital rates. For instance, we might not believe that 'growth' to a
+#' dormant stage class really reflects biological growth, in which case we could
+#' exclude transitions \emph{to} the dormant stage class using the argument
+#' \code{exclude_row}. We may or may not want to ignore 'growth' transitions
+#' \emph{from} the dormant stage class, which can be done using
+#' \code{exclude_col}. The argument \code{exclude_col} effectively just coerces
+#' the respective vital rate to \code{NA}, to prevent it from getting used in
+#' subsequent calculations.
 #' 
 #' @author Patrick Barks <patrick.barks@@gmail.com>
 #' 
@@ -96,108 +110,147 @@ NULL
 
 #' @rdname vr_vec
 #' @export vr_vec_survival
-vr_vec_survival <- function(matU, posU = matU > 0, stage_exclude = NULL) {
+vr_vec_survival <- function(matU,
+                            posU = matU > 0,
+                            exclude_col = NULL) {
   
   checkValidMat(matU)
   pos_vital <- apply(posU, 2, any)
   
   v <- colSums(matU)
   v[!pos_vital] <- NA_real_
-  v[stage_exclude] <- NA_real_
+  v[exclude_col] <- NA_real_
   return(v)
 }
 
 
 #' @rdname vr_vec
 #' @export vr_vec_growth
-vr_vec_growth <- function(matU, posU = matU > 0, stage_exclude = NULL,
+vr_vec_growth <- function(matU,
+                          posU = matU > 0,
+                          exclude_row = NULL,
+                          exclude_col = NULL,
                           surv_only_na = TRUE) {
   
-  vmat <- vr_mat_U(matU, posU = posU, surv_only_na = surv_only_na)
+  vmat <- vr_mat_U(matU = matU,
+                   posU = posU,
+                   surv_only_na = surv_only_na)
+  
   tri_low <- lower.tri(vmat, diag = FALSE)
   vmat[!tri_low] <- NA_real_
   
-  if (!is.null(stage_exclude)) vmat[stage_exclude, ] <- NA_real_
+  vmat[exclude_row, ] <- NA_real_
   
   v <- colSums2(vmat)
-  v[stage_exclude] <- NA_real_
+  v[exclude_col] <- NA_real_
+  
   return(v)
 }
 
 
 #' @rdname vr_vec
 #' @export vr_vec_shrinkage
-vr_vec_shrinkage <- function(matU, posU = matU > 0, stage_exclude = NULL,
+vr_vec_shrinkage <- function(matU,
+                             posU = matU > 0,
+                             exclude_row = NULL,
+                             exclude_col = NULL,
                              surv_only_na = TRUE) {
   
-  vmat <- vr_mat_U(matU, posU = posU, surv_only_na = surv_only_na)
+  vmat <- vr_mat_U(matU = matU,
+                   posU = posU,
+                   surv_only_na = surv_only_na)
+  
   tri_upp <- upper.tri(vmat, diag = FALSE)
   vmat[!tri_upp] <- NA_real_
   
-  if (!is.null(stage_exclude)) vmat[stage_exclude, ] <- NA_real_
+  vmat[exclude_row, ] <- NA_real_
   
   v <- colSums2(vmat)
-  v[stage_exclude] <- NA_real_
+  v[exclude_col] <- NA_real_
+  
   return(v)
 }
 
 
 #' @rdname vr_vec
 #' @export vr_vec_stasis
-vr_vec_stasis <- function(matU, posU = matU > 0, stage_exclude = NULL,
+vr_vec_stasis <- function(matU,
+                          posU = matU > 0,
+                          exclude_col = NULL,
                           surv_only_na = TRUE) {
   
-  vmat <- vr_mat_U(matU, posU = posU, surv_only_na = surv_only_na)
-  pos_vital <- diag(posU)
+  vmat <- vr_mat_U(matU = matU,
+                   posU = posU,
+                   surv_only_na = surv_only_na)
   
   v <- diag(vmat)
-  v[stage_exclude] <- NA_real_  # set excluded stages to NA
-  v[!pos_vital] <- NA_real_     # set stages with no possible stasis to NA
+  v[exclude_col] <- NA_real_
+  
+  pos_vital <- diag(posU)
+  v[!pos_vital] <- NA_real_
+  
   return(v)
 }
 
 
 #' @rdname vr_vec
 #' @export vr_vec_dorm_enter
-vr_vec_dorm_enter <- function(matU, posU = matU > 0, dorm_stages) {
+vr_vec_dorm_enter <- function(matU,
+                              posU = matU > 0,
+                              dorm_stages) {
   
-  vmat <- vr_mat_U(matU, posU = posU)
-  pos_vital <- apply(posU[dorm_stages, , drop = FALSE], 2, any)
+  vmat <- vr_mat_U(matU = matU,
+                   posU = posU)
   
   v <- colSums(vmat[dorm_stages, , drop = FALSE], na.rm = TRUE)
   v[dorm_stages] <- NA_real_
+  
+  pos_vital <- apply(posU[dorm_stages, , drop = FALSE], 2, any)
   v[!pos_vital] <-  NA_real_
+  
   return(v)
 }
 
 
 #' @rdname vr_vec
 #' @export vr_vec_dorm_exit
-vr_vec_dorm_exit <- function(matU, posU = matU > 0, dorm_stages) {
+vr_vec_dorm_exit <- function(matU,
+                             posU = matU > 0,
+                             dorm_stages) {
   
-  vmat <- vr_mat_U(matU, posU = posU)
+  vmat <- vr_mat_U(matU = matU,
+                   posU = posU)
+  
+  vmat[dorm_stages, ] <- 0
+  v <- colSums(vmat)
   
   # possible transitions from dormant to non-dormant
   pos_exit <- matrix(FALSE, nrow = nrow(matU), ncol = nrow(matU))
   pos_exit[-dorm_stages, dorm_stages] <- TRUE
   pos_vital <- apply(posU & pos_exit, 2, any)
-  
-  vmat[dorm_stages, ] <- 0
-  v <- colSums(vmat)
   v[!pos_vital] <- NA_real_
+  
   return(v)
 }
 
 
 #' @rdname vr_vec
 #' @export vr_vec_fecund
-vr_vec_fecund <- function(matU, matR, posR = matR > 0, weights = NULL) {
+vr_vec_fecund <- function(matU,
+                          matR,
+                          posR = matR > 0,
+                          weights_row = NULL) {
   
-  vmat <- vr_mat_R(matU, matR, posR = posR)
+  vmat <- vr_mat_R(matU = matU,
+                   matR = matR,
+                   posR = posR)
+  
+  if (is.null(weights_row)) weights_row <- rep(1, nrow(matU))
+  v <- colSums2(vmat * weights_row)
+  
   pos_vital <- apply(posR, 2, any)
+  v[!pos_vital] <- NA_real_
   
-  if (is.null(weights)) weights <- rep(1, nrow(matU))
-  v <- colSums2(vmat * weights)
   return(v)
 }
 
