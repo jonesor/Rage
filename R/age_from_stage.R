9015a4fb3ea1be2ab982fdc63a52fe9646f73b39
patrickbarks <patrick.barks@gmail.com>
2019-02-13 15:38:52 +0100

    update seealso link to lifetable_convert

--- R/age_from_stage.R
+++ R/age_from_stage.R
@@ -23,7 +23,7 @@
 #' @author Owen R. Jones <jones@@biology.sdu.dk>
 #' @author Patrick Barks <patrick.barks@@gmail.com>
 #' 
-#' @seealso \code{\link{surv_conversion}}
+#' @seealso \code{\link{lifetable_convert}}
 #' 
 #' @references Caswell, H. (2001) Matrix Population Models: Construction,
 #'   Analysis, and Interpretation. Sinauer Associates; 2nd edition. ISBN:
