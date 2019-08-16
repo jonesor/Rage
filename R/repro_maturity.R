832a1388594dacb0b5811f61b5fd88de1ebef7c8
patrickbarks <patrick.barks@gmail.com>
2019-08-16 15:08:02 +0200

    add fn mature_distrib to calculate distrib of repro maturity among stage classes (toward #53)

--- R/repro_maturity.R
+++ R/repro_maturity.R
@@ -5,8 +5,10 @@
 #' trajectory of reproduction for individuals in a matrix population model.
 #' Includes functions to calculated the probability of achieving reproductive
 #' maturity (\code{mature_prob}), mean age at first reproduction
-#' (\code{mature_age}), and remaining life expectancy at the age of first
-#' reproduction (\code{mature_life_expect}).
+#' (\code{mature_age}), remaining life expectancy at the age of first
+#' reproduction (\code{mature_life_expect}), and distribution of individuals
+#' first achieving reproductive maturity among stage class
+#' (\code{mature_distrib}).
 #' 
 #' @param matU The survival component of a matrix population model (i.e. a
 #'   square projection matrix reflecting survival-related transitions; e.g.
@@ -16,8 +18,12 @@
 #'   sexual, clonal, or both)
 #' @param start The index of the first stage at which the author considers
 #'   the beginning of life. Defaults to 1.
+#' @param repro_stages Logical vector indicating which stages are reproductive.
+#'   Must be of length \code{ncol(matU)}.
 #'   
-#' @return Trait value (scalar)
+#' @return For \code{mature_distrib}, a vector giving the proportion of
+#'   individuals that first reproduce within each stage class. For all others, a
+#'   scalar trait value.
 #' 
 #' @author Roberto Salguero-Gomez <rob.salguero@@zoo.ox.ac.uk>
 #' @author Hal Caswell <hcaswell@@whoi.edu>
@@ -29,23 +35,18 @@
 #'   978-0878930968
 #'   
 #' @examples
-#' matU <- rbind(c(0.1,   0,   0,   0),
-#'               c(0.5, 0.2, 0.1,   0),
-#'               c(  0, 0.3, 0.3, 0.1),
-#'               c(  0,   0, 0.5, 0.6))
+#' data(mpm1)
 #' 
-#' matF <- rbind(c(  0,   0, 1.1, 1.6),
-#'               c(  0,   0, 0.8, 0.4),
-#'               c(  0,   0,   0,   0),
-#'               c(  0,   0,   0,   0))
+#' mature_prob(mpm1$matU, mpm1$matF, start = 2)
+#' mature_age(mpm1$matU, mpm1$matF, start = 2)
+#' mature_life_expect(mpm1$matU, mpm1$matF, start = 2)
 #' 
-#' mature_prob(matU, matF, start = 1)
-#' mature_age(matU, matF, start = 1)
-#' mature_life_expect(matU, matF, start = 1)
+#' ### distribution of first reproductive maturity among stage classes
+#' repstage <- id_repro_stages(mpm1$matF)
+#' mature_distrib(mpm1$matU, start = 2, repro_stages = repstage)
 #' 
 #' @name repro_maturity
 NULL
-
 
 
 #' @rdname repro_maturity
@@ -63,7 +64,6 @@ mature_prob <- function(matU, matR, start = 1L) {
   
   return(Bprime[2, start])
 }
-
 
 
 #' @rdname repro_maturity
@@ -93,7 +93,6 @@ mature_age <- function(matU, matR, start = 1L) {
 }
 
 
-
 #' @rdname repro_maturity
 #' @importFrom MASS ginv
 #' @export mature_life_expect
@@ -106,6 +105,35 @@ mature_life_expect <- function(matU, matR, start = 1L) {
   return(l0 - mean_age_mature)
 }
 
+
+#' @rdname repro_maturity
+#' @export mature_distrib
+mature_distrib <- function(matU, start = 1L, repro_stages) {
+  
+  # validate arguments
+  checkValidMat(matU, warn_surv_issue = TRUE)
+  checkValidStartLife(start, matU)
+  if (ncol(matU) != length(repro_stages)) {
+    stop("length(repro_stages) must equal ncol(matU)", call. = FALSE)
+  }
+  
+  if (sum(repro_stages) == 1) {
+    n1 <- as.numeric(repro_stages)
+  } else {
+    primeU <- matU
+    primeU[,repro_stages] <- 0
+    N <- try(solve(diag(nrow(primeU)) - primeU))
+    if (class(N) == "try-error") {
+      stop("Cannot derive fundamental matrix because matU is non-invertable",
+           .call = FALSE)
+    } else {
+      n1 <- rep(0, nrow(matU))
+      n1[repro_stages] <- N[repro_stages,start] / sum(N[repro_stages,start])
+    }
+  }
+  names(n1) <- colnames(matU)
+  return(n1)
+}
 
 
 #' @noRd
---
+++ tests/testthat/test-mature_distrib.R
@@ -0,0 +1,22 @@
+context("mature_distrib")
+
+test_that("mature_distrib works correctly", {
+  
+  repstages <- id_repro_stages(mat_f)
+  x1 <- mature_distrib(mat_u, repro_stages = repstages)
+  expect_length(x1, ncol(mat_u))
+  expect_equal(sum(x1), 1)
+  expect_length(x1[x1 > 0], 1) # repro maturity in only 1 stage class
+  
+  mat_u2 <- mat_u
+  mat_u2[4,2] <- 0.1
+  x2 <- mature_distrib(mat_u2, repro_stages = repstages)
+  expect_length(x2[x2 > 0], 2) # repro maturity in 2 stage classes
+})
+
+test_that("mature_distrib warns and fails gracefully", {
+  
+  repstages <- id_repro_stages(mat_f)
+  expect_error(mature_distrib(mat_u_na, repro_stages = repstages))
+  expect_error(mature_distrib(mat_u, start = 10, repro_stages = repstages))
+})
