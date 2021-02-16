18dc962aa8bea7c08981f39e0736f4264109712c
Owen Jones <jones@biology.sdu.dk>
2021-02-16 15:13:39 +0100

    Checked and corrected consistency of function documentation

--- R/data.R
+++ R/data.R
@@ -1,14 +1,14 @@
 #' Example matrix population model (MPM)
 #' 
 #' @description
-#' An example matrix population model (MPM) used for demonstration and testing.
-#' Consists of five stage classes: 'seed', 'small', 'medium', 'large', and
-#' 'dormant'.
+#' An example matrix population model (MPM) used for demonstration and testing
+#' purposes. The MPM consists of five stage classes: 'seed', 'small', 'medium',
+#' 'large', and 'dormant'.
 #' 
 #' @format
 #' A list with two elements:
 #' \describe{
-#'   \item{matU}{The survival-related component of the MPM}
-#'   \item{matF}{The sexual reproduction component of the MPM}
+#'   \item{matU}{The survival-related component of the MPM.}
+#'   \item{matF}{The sexual reproduction component of the MPM.}
 #' }
 "mpm1"
