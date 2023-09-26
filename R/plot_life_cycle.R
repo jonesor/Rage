f7feb6297749c7dfe6fca78cba739bdb15cb9d4f
Owen Jones <jonesor@gmail.com>
2023-09-26 22:22:11 +0200

    plot life cycle updates
    Now ensures correct order of nodes horizontally. Also allows arbitrary ordering of nodes.

--- R/plot_life_cycle.R
+++ R/plot_life_cycle.R
@@ -15,6 +15,9 @@
 #' @param fontsize Size of the font used in the diagram.
 #' @param nodefontsize Size of the font used in the node part of the diagram.
 #' @param edgecol Colour of the arrows in the diagram.
+#' @param node_order An optional numeric vector giving the order that the nodes
+#'   should be presented in the plot. Default is `NULL` whereby the order is the
+#'   same as `stages`, or row/column names, of the matrix.
 #'
 #' @return An object of class \code{grViz} representing the life cycle diagram
 #'
@@ -33,17 +36,23 @@
 #'
 #' plot_life_cycle(matA)
 #'
+#' # Change the order of the nodes and give them names
+#' plot_life_cycle(matA,
+#'   stages = c("A", "B", "C", "D", "E"),
+#'   node_order = 5:1
+#' )
+#'
 #' @importFrom DiagrammeR grViz
 #' @export plot_life_cycle
 plot_life_cycle <- function(matA, stages, title = NULL, shape = "egg",
                             fontsize = 10, nodefontsize = 12,
-                            edgecol = "grey") {
+                            edgecol = "grey", node_order = NULL) {
   # Identify stages
   if (missing(stages) && is.null(dimnames(matA))) {
     stages <- seq_len(ncol(matA))
   } else if (missing(stages) && !is.null(dimnames(matA))) {
     stages <- dimnames(matA)[[1]]
-    
+
     if (!identical(
       dimnames(matA)[[1]],
       dimnames(matA)[[2]]
@@ -55,30 +64,44 @@ plot_life_cycle <- function(matA, stages, title = NULL, shape = "egg",
       ))
     }
   }
-  
+
+  if (length(stages) != nrow(matA)) {
+    stop("The length of stages does not equal the dimension of matA")
+  }
+
+  if (!is.null(node_order)) {
+    if (length(node_order) != nrow(matA)) {
+      stop("The length of node_order does not equal the dimension of matA")
+    }
+  }
   # Construct a "from" -> "to" graph dataset (edges)
   graph <- expand.grid(to = stages, from = stages)
   graph$trans <- round(c(matA), 3)
-  
+
   # Subset to only include those where the trans > 0
   graph <- graph[graph$trans > 0, ]
-  
+
   # Create vector of node names (add semicolon for use by graphViz)
-  nodes <- paste(paste0("'", stages, "'"), collapse = "; ")
-  
+  if (is.null(node_order)) {
+    nodes <- paste(paste0("'", stages, "'"), collapse = " -> ")
+  } else {
+    stages <- stages[order(node_order)]
+    nodes <- paste(paste0("'", stages, "'"), collapse = " -> ")
+  }
+  nodes <- paste0(nodes, " [style=invis]")
+
   # Manipulate minimim length of edge to make the plot pretty (experimental!)
   graph$min_len <- (as.numeric(graph$to) - as.numeric(graph$from)) * 3
-  
   # Create the edges argument for graphviz by pasting commands together
   edges <- paste0("'", graph$from, "'", " -> ", "'", graph$to, "'",
-                  "[minlen=", graph$min_len,
-                  ",fontsize=", fontsize,
-                  ",color=", edgecol,
-                  ",xlabel=", paste("\"", graph$trans),
-                  "\"]\n",
-                  collapse = ""
+    "[minlen=", graph$min_len,
+    ",fontsize=", fontsize,
+    ",color=", edgecol,
+    ",xlabel=", paste("\"", graph$trans),
+    "\"]\n",
+    collapse = ""
   )
-  
+
   # The graphviz argument, pasted together
   grViz(
     paste(
